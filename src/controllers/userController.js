2bf26a87163bbf16ff2edbcc9cecbe65352de156
Abeer <abeer.technology@gmail.com>
2022-09-01 15:44:27 +0600

    adding confirmEmail controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -45,3 +45,20 @@ exports.registerUser = asyncErrorHandler(async (req, res, next) => {
     })
   }
 })
+
+// Confirm Email after signup
+exports.confirmEmail = asyncErrorHandler(async (req, res, next) => {
+  const q = query(Users, where("email", "==", req.user.email))
+  const docSnap = await getDocs(q)
+
+  if (docSnap.docs.length === 0) {
+    return next(new ErrorHandler("User not found", 404))
+  }
+
+  // Get Confirm Email Token
+
+  res.status(200).json({
+    success: true,
+    message: `Email sent to ${docSnap.docs[0].data().email} successfully`,
+  })
+})
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express")
-const { registerUser } = require("../controllers/userController")
+const { registerUser, confirmEmail } = require("../controllers/userController")
 const validatePassword = require("../middlewares/validatePassword")
 
 const router = express.Router()
 
 // router.route("/register").post(registerUser)
 router.route("/register").post(validatePassword, registerUser)
+
+router.route("/confirm-email").post(confirmEmail)
+// router.route("/confirm-email").post(checkAuth, confirmEmail)
 
 module.exports = router
