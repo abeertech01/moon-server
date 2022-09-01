96265c1353a8676f9569368388399f5a6d772e23
Abeer <abeer.technology@gmail.com>
2022-09-01 19:06:09 +0600

    added session

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+)
 
 app.get("/ingredients", (req, res) => {
   res.json({
