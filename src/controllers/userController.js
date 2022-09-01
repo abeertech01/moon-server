5a2b5dd7adb824d99e838e6b63e967fd8ec5b807
Abeer <abeer.technology@gmail.com>
2022-09-01 14:14:52 +0600

    lil change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,14 @@
 const asyncErrorHandler = require("../middlewares/asyncErrorHandler")
+const ErrorHandler = require("../utils/errorHandler")
 
 // Register a user
 exports.registerUser = asyncErrorHandler((req, res, next) => {
   // ----- Check if the email is associated to an existing account -----
   const { email, password } = req.body
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Each field needs to be fulfilled", 409))
+  }
 
   res.json({
     msg: "register controller",
