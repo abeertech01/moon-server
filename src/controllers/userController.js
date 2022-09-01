00e1abe22926d7eda981c5c33b86f53443656889
Abeer <abeer.technology@gmail.com>
2022-09-01 14:25:14 +0600

    commented out qSnapshot conditions and added async notation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ const asyncErrorHandler = require("../middlewares/asyncErrorHandler")
 const ErrorHandler = require("../utils/errorHandler")
 
 // Register a user
-exports.registerUser = asyncErrorHandler((req, res, next) => {
+exports.registerUser = asyncErrorHandler(async (req, res, next) => {
   // ----- Check if the email is associated to an existing account -----
   const { email, password } = req.body
 
