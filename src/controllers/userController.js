c5fddf4406ecb769a4b7d2a73b16d43589d4097b
Abeer <abeer.technology@gmail.com>
2022-09-01 14:08:42 +0600

    some minor edit in register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,9 @@ const asyncErrorHandler = require("../middlewares/asyncErrorHandler")
 
 // Register a user
 exports.registerUser = asyncErrorHandler((req, res, next) => {
+  // ----- Check if the email is associated to an existing account -----
+  const { email, password } = req.body
+
   res.json({
     msg: "register controller",
   })
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,11 +6,4 @@ const router = express.Router()
 router.route("/register").post(registerUser)
 // router.route("/register").post(validatePassword, registerUser)
 
-//test
-router.get("/get", (req, res, next) => {
-  res.json({
-    success: true,
-  })
-})
-
 module.exports = router
