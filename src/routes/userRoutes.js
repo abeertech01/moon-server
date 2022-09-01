bbeb2f619b39ce4d256ea224f00b1f9577041743
Abeer <abeer.technology@gmail.com>
2022-09-01 15:32:09 +0600

    added validatePassword

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const { registerUser } = require("../controllers/userController")
+const validatePassword = require("../middlewares/validatePassword")
 
 const router = express.Router()
 
-router.route("/register").post(registerUser)
-// router.route("/register").post(validatePassword, registerUser)
+// router.route("/register").post(registerUser)
+router.route("/register").post(validatePassword, registerUser)
 
 module.exports = router
